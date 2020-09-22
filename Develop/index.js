5de82fa3f58c1a5df9c3e393f3b2051aa8d2577c
Slothing <seth.r.martineau@gmail.com>
2020-09-21 19:57:28 -0600

    added my questions

--- Develop/index.js
+++ Develop/index.js
@@ -6,7 +6,46 @@ const fs = require("fs");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is your repository called?",
+        name: "repository"
+    },
+    {
+        type: "input",
+        message: "What is your project description?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What installations are needed to run your project?",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "Write instructions to use your project?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "If there were contributors to this project please enter them here, if there are multiple contributors, separate their names with a comma and no space.",
+        name: "contributors"
+    },
+    {
+        type: "input",
+        message: "Provide examples on how to run tests.",
+        name: "test"
+    },
 ];
 
 // function to write README file
